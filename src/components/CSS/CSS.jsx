c9c0efc8731c5ff78aba565f91783ca22faaa509
abinayakathiresan <abinayakathiresan7@gmail.com>
2024-04-23 13:22:05 +0530

    commit

--- src/components/CSS/CSS.jsx
+++ src/components/CSS/CSS.jsx
@@ -17,6 +17,12 @@ export const CSS = ()=> {
             <div><span>Fixed:</span> it allow us to use top, bottom, left, right properties. It wont reserves the space and takes element out of document flow. It align the element to its body tag. It wont hide when we scroll. Similar to Abolute, but it wont hide when we scroll. </div>
             <div><span>Sticky:</span>similar to Absolute, but it become fixed when element reach top of the page when we scroll.
             </div>
+            <div><span>Static:</span> it wont allow us to use top, bottom, left, right properties. It keeps element in document flow.</div>
+            <div><span>Relative:</span> it allow us to use top, bottom, left, right properties. It reserves the space and takes element out of document flow.</div>
+            <div><span>Absolute:</span> it allow us to use top, bottom, left, right properties. It wont reserves the space and takes element out of document flow. It align the element to its body tag or nearest non-static element </div>
+            <div><span>Fixed:</span> it allow us to use top, bottom, left, right properties. It wont reserves the space and takes element out of document flow. It align the element to its body tag. It wont hide when we scroll. Similar to Abolute, but it wont hide when we scroll. </div>
+            <div><span>Sticky:</span>similar to Absolute, but it become fixed when element reach top of the page when we scroll.
+            </div>
         </div>
         </>
     )
